03d6b178e2089d979abeb093d573e04699de4c58
青云 付 <1366773912@qq.com>
2021-12-23 22:50:25 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日");
+//	case 6:
+//	case 7:
+//		printf("休息日");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	while (i <= 1000000)
+//	{
+//		printf("%d ", i);
+//		i++;
+//
+//	}
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	for (;a <= 100;a++)
+	{
+		printf("%d ", a);
+	}
+	return 0;
+}
